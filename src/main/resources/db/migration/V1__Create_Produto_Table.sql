5b7ec59f31c68619505f95bd85612376a5d4b88b
alexiaev20 <alexiaevellyn20@gmail.com>
2026-09-04 16:39:59 -0300

    feat: clean architecture, flyway, remove json redundante e paginaçao real

---
+++ src/main/resources/db/migration/V1__Create_Produto_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE produto (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(255) NOT NULL,
+    quantidade INT NOT NULL,
+    preco DOUBLE NOT NULL
+);
